87b76a4a7762411293a0d75c3b9163be71db425a
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-07 10:38:50 +0530

    [Sanju] ADD : Check present or absent

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,6 +7,22 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("********Welome to Employee Wage Program***********");
+            //Constants
+            int PRESENT = 1;
+
+            Random random = new Random();
+            int employeeCheck = random.Next(0, 2);
+
+            Console.WriteLine("random value " + employeeCheck);
+            if (employeeCheck == PRESENT)
+            {
+                Console.WriteLine("Employee is Present.");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
             Console.ReadLine();
         }
     }
