4d4acb8b2f59aea7804f90adff7f4fad5af80ecc
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-07 10:42:26 +0530

    [Sanju] ADD : Employee wage for a day

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,20 +9,24 @@ namespace EmployeeWage
             Console.WriteLine("********Welome to Employee Wage Program***********");
             //Constants
             int PRESENT = 1;
-
+            int EMP_RATE_PER_HOUR = 20;
+            //local variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int employeeCheck = random.Next(0, 2);
 
             Console.WriteLine("random value " + employeeCheck);
             if (employeeCheck == PRESENT)
             {
-                Console.WriteLine("Employee is Present.");
-
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent.");
+                empHrs = 0;
             }
+            empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee wage per day is : " + empWage);
             Console.ReadLine();
         }
     }
