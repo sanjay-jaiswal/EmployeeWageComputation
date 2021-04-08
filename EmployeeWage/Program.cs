ea9e882ffac494567ab1e57c551064c90ad8b2dd
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-08 19:50:47 +0530

    [Sanju] ADD : added part time

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,24 +7,31 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("********Welome to Employee Wage Program***********");
+
             //Constants
-            int PRESENT = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
             //local variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int employeeCheck = random.Next(0, 2);
+            int employeeCheck = random.Next(0, 3);
 
             Console.WriteLine("random value " + employeeCheck);
-            if (employeeCheck == PRESENT)
+            if (employeeCheck == FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (employeeCheck == PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
                 empHrs = 0;
             }
+
             empWage = EMP_RATE_PER_HOUR * empHrs;
             Console.WriteLine("Employee wage per day is : " + empWage);
             Console.ReadLine();
