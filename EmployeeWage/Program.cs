40033a1d72634a52296c0855598331c4ca7d654c
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-08 22:43:31 +0530

    [Sanju] ADD : Calculate emp wage till days or hrs reached

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,45 +5,41 @@ namespace EmployeeWage
 {
     class Program
     {
-        // constants declaration
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
+        //Constants declaration
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAY = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
 
-        /// <summary>
-        /// Calculate emp wage using case statements
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
             Console.WriteLine("********Welome to Employee Wage Program***********");
-
             //Variables declaration
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            //Calculating wages
-
-            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int employeeCheck = random.Next(0, 3);
                 switch (employeeCheck)
                 {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case PART_TIME:
+                    case IS_PART_TIME:
                         empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee wage is : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days is : " + totalWorkingDays + " " + "Employee Hours is :" + empHrs);
             }
-            Console.WriteLine("Employee's wage monthly is : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total employee wage is : " + totalEmpWage);
 
         }
     }
