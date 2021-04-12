bc0b9b0aa72573bdecde239f28554466cf480f50
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-12 21:14:37 +0530

    [Sanju] ADD : compute employee wage for multiple companies using methods

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,15 +9,11 @@ namespace EmployeeWage
             Console.WriteLine("********Welome to Employee Wage Program***********");
             Console.WriteLine("=====================================================================================================");
 
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
-            EmpWageBuilderObject phonePay = new EmpWageBuilderObject("PhonePay", 15, 5, 22);
-            dMart.computeEmpwage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpwage();
-            Console.WriteLine(reliance.toString());
-            phonePay.computeEmpwage();
-            Console.WriteLine(phonePay.toString());
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilderArray.addCompanyEmpWage("PhonePay", 15, 5, 22);
+            empWageBuilderArray.computeEmpWage();
         }       
     }
 }
