e0c4959ea9d87a14f68453552dc736149e89c6bf
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-12 10:22:16 +0530

    [Sanju] ADD : Calculate emp wage using class method.

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,46 +1,13 @@
-﻿
-using System;
+﻿using System;
 
 namespace EmployeeWage
 {
     class Program
     {
-        //Constants declaration
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
         static void Main(string[] args)
         {
-
             Console.WriteLine("********Welome to Employee Wage Program***********");
-            //Variables declaration
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int employeeCheck = random.Next(0, 3);
-                switch (employeeCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days is : " + totalWorkingDays + " " + "Employee Hours is :" + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total employee wage is : " + totalEmpWage);
-
-        }
+            ComputeEmploeeWage.computeEmpWage();
+        }       
     }
 }
